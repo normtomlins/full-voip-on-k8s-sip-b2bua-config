878f7b56d71491d2b4d3ee5ca1854e5ba0208bd3
normtomlins <norm@officelink.com>
2024-02-26 12:25:26 +0800

    Update startup.sh
    Using a public service to get the IP address of the external interface,  might need to do something different for cloud hosted systems.

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
-PUB_IP=$(wget -q -O - http://169.254.169.254/latest/meta-data/public-ipv4)
+PUB_IP=$(curl ipconfig.org)
 sed -i.original 's/{{ PUBLIC_IP }}/'$PUB_IP'/g' /etc/freeswitch/sip_profiles/external.xml
 /usr/bin/freeswitch -u freeswitch -g freeswitch -c
